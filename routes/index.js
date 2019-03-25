73826cbfe7a3e25db24e833272d42d075940d46b
HawiCaesar <brian.odhiambo@andela.com>
2019-03-25 23:03:39 +0300

    feat(contact): a user can read a contact

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createContact,
   getAllContacts,
+  getOneContact,
   updateContacts
 } from "../controllers/contact";
 
@@ -9,6 +10,7 @@ const Route = express.Router();
 
 Route.post("/contacts", createContact);
 Route.get("/contacts", getAllContacts);
+Route.get("/contacts/:contactId", getOneContact);
 Route.put("/contacts/:contactId", updateContacts);
 
 export default Route;
--- tests/fixtures.js
+++ tests/fixtures.js
@@ -1,4 +1,18 @@
-export const validContact = {
-  name: "John Brown",
-  phone: "872378328"
-};
+export default [
+  {
+    name: "John Brown",
+    phone: "872378328"
+  },
+  {
+    name: "Derren Green",
+    phone: "101201212"
+  },
+  {
+    name: "Jasmine Haddish",
+    phone: "1890923"
+  },
+  {
+    name: "Jelly Waldo",
+    phone: "8901234565895489"
+  }
+];
