a5c6b4f1b0cc15947a4121c898efebedd6ba98e8
HawiCaesar <brian.odhiambo@andela.com>
2019-03-27 10:47:29 +0300

    chore(config): make config cleaner

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const devUsername = process.env.DATABASE_USERNAME;
-const devPassword = process.env.DATABASE_PASSWORD;
-const devDatabase = process.env.DATABASE;
-const testUsername = process.env.TEST_DATABASE_USERNAME;
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const database = process.env.DATABASE;
+const testUser = process.env.TEST_DATABASE_USERNAME;
 const testPassword = process.env.TEST_DATABASE_USERNAME;
 const testDatabase = process.env.TEST_DATABASE;
 
 const config = {
   development: {
-    username: devUsername,
-    password: devPassword,
-    database: devDatabase,
+    username,
+    password,
+    database,
     host: "127.0.0.1",
     port: 5432,
     dialect: "postgres"
   },
   test: {
-    username: testUsername,
+    username: testUser,
     password: testPassword,
     database: testDatabase,
     host: "127.0.0.1",
